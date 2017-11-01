40cb672028feae3dfa32a63b18561bd3ceebb6fe
Junshen Chen <juc115@ucsd.edu>
2017-10-31 18:27:45 -0700

    add firebase credential to App.js ctor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        /*the following is firebase config*/
+        var config = {
+            apiKey: "AIzaSyABmBOMLTEGtBLrjkwcDu9ab0ExE208R-4",
+            authDomain: "friend-zone-9219b.firebaseapp.com",
+            databaseURL: "https://friend-zone-9219b.firebaseio.com",
+            projectId: "friend-zone-9219b",
+            storageBucket: "",
+            messagingSenderId: "1002501074461"
+        };
+        firebase.initializeApp(config);
+        /*the above is firebase config*/
+
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
