5a608f5c88dd485f47d91581bfc8b455f2fee17e
Junshen Chen <juc115@ucsd.edu>
2017-11-09 16:54:39 -0800

    fixed Enrollment.js

--- src/dao/Enrollment.js
+++ src/dao/Enrollment.js
@@ -13,13 +13,29 @@ class Enrollment {
         this.enrolled_users = user_list;
     }
 
-    // Adding a User_id to enrolled class
+    /*
+    * add_user(user_id);
+    * where user_id is the user to add to the course described by "this"
+    * there is no callback function because it will always succeed */
+    add_user(user_id){
 
-    // Remove a User_id in enrolled class
+    }
 
-    /* add self to table on Firebase */
+    /*
+    * remove_user(user_id, callback)
+    * where user_id is the user to remove from the course described by "this"
+    * where callback(err, data) is a function expecting 2 params:
+    *   err: null - if no error
+    *        { msg: "Enrollment.remove_user(): user_id DNE" } - if error (in this case user_id not in enrolled_users)
+    *   data: null - if any error
+    *         { msg: "Enrollment.remove_user(): remove success: } - if no error */
+    remove_user(user_id, callback){
+
+    }
+
+    /* add/update self to table on Firebase */
     push(){
-        firebase.database().ref('Enrolled_Users').child(this.course_id).set({
+        firebase.database().ref('Enrollment').child(this.course_id).set({
            course_id: this.course_id,
            enrolled_users: this.enrolled_users
         });
