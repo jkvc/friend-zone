01cb67c3969f97d3f5e784807ba5ccea180c262b
Alejandro Toy <AlejandroT9900@gmail.com>
2017-11-09 16:27:16 -0800

    Enrollment DAO, push added

---
+++ src/dao/Enrollment.js
@@ -0,0 +1,29 @@
+import md5 from 'md5';
+import firebase from 'firebase';
+
+/* Class contains Enrollment object associated to a Course */
+/* Contains the course_id and an expandable, reducible list of users enrolled in the course */
+class Enrollment {
+
+    course_id;
+    enrolled_users; /* Stores hashed user_id's in an array*/
+
+    constructor(course, user_list) {
+        this.course_id = course;
+        this.enrolled_users = user_list;
+    }
+
+    // Adding a User_id to enrolled class
+
+    // Remove a User_id in enrolled class
+
+    /* add self to table on Firebase */
+    push(){
+        firebase.database().ref('Enrolled_Users').child(this.course_id).set({
+           course_id = this.course_id,
+           enrolled_users = this.enrolled_users
+        });
+    }
+}
+
+export default Enrollment;
