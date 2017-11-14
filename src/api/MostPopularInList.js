470866e4b46465e259953c262ee9135c9bad34f3
Madaoism <cai_yiming@yahoo.com>
2017-11-13 23:44:51 -0800

    Finished the MostPopularInList function

--- src/api/MostPopularInList.js
+++ src/api/MostPopularInList.js
@@ -25,13 +25,74 @@
 export function most_popular_in_list(param, callback){
 
     /*handle some error*/
+    if ( !("count" in param && "list" in param) )
+    {
+        callback(new Error("Missing states in param to most_popular_in_list", 9));
+    }
+    if (param.count < 1 || param.list.length == 0)
+    {
+        callback(new Error("Invalid Inputs to most_popular_in_list", 11));
+    }
+    if (typeof param.count !== 'number')
+    {
+        callback(new Error("param.count is not a number!", 13));
+    }
+    if (typeof param.list !== 'object' || param.list.constructor !== Array)
+    {
+        callback(new Error("param.list is not an array!", 15));
+    }
 
     var count = param.count;
     var list = param.list;
     var result = [];
+    var dict = {};
 
     /*do something with result*/
 
+    // For each list
+    for (var i = 0; i < list.length; i++)
+    {
+        // For each list in the list
+        for (var j = 0; j < list[i].length; j++)
+        {
+
+            // Add a new student into the list, with count of 1
+            if ( ! (list[i][j] in dict) )
+            {
+                dict [list[i][j]] = 1;
+            }
+
+            // Increment the student count by 1 if he is already added
+            else
+            {
+                dict [list[i][j]] += 1;
+            }
+        }
+    }
+
+    // For each student in the dict, push the [sameClassCount, studentName] onto the array
+    var array = [];
+    for (var key in dict)
+    {
+        array.push( [dict[key], key] );
+    }
+
+    // Sort the [sameClassCount, studentName] by:
+    //  1. their sameClassCount in decreasing order
+    //  2. their studentName in increasing alphabetical order
+    array.sort(function(a, b)
+    {
+        if (a[0] == b[0])
+        {
+            return a[1] > b[1];
+        }
+        else return a[0] < b[0];
+    });
+
+    for (var i = 0; i < count; i++)
+    {
+        result.push(array[i][1]);
+    }
 
     callback(null, result);
 }
