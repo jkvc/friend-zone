a2b2ace69bb5645ea47588285c5d1d08566fec46
yiming-cai <32525453+yiming-cai@users.noreply.github.com>
2017-11-14 21:50:34 -0800

    fixed index

--- src/index.js
+++ src/index.js
@@ -26,14 +26,14 @@ registerServiceWorker();
 
 // Using a callback method (observer) avoids the issue that when web are in
 // process of getting the user, user is null even though user has signed in.
-/*
+
 firebase.auth().onAuthStateChanged((user) => {
     if (user)
         ReactDOM.render(<MainLayout/>, document.getElementById('root'));
     else
         ReactDOM.render(<Welcome/>, document.getElementById('root'));
 });
-*/
+
 /*
 * TODO: Yiming: comment out line 15-35, uncomment line 36, npm start, press F12 in chrome and look at console to see Unit test result
 * */
@@ -42,10 +42,10 @@ firebase.auth().onAuthStateChanged((user) => {
 /*
 * TODO: To test the calendar, comment out Line 15-35, uncomment out the following lines of code
  */
-
+/*
 var events = [
     {"days":"MWF", "hours":["11:30", "12:30"], "title":"CSE11" },
     {"days":"TuTh", "hours":["10:20", "11:20"], "title":"CSE100"},
-    {"days":"MWTu", "hours":["00:10", "11:00"], "title":"Chilling"}
 ];
 ReactDOM.render(<CalendarHelper events={events}/>, document.getElementById('root'));
+*/
