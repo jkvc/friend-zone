b54d852e52d3dba88e6580f3730afda564c70e4c
yiming-cai <32525453+yiming-cai@users.noreply.github.com>
2017-11-14 23:35:05 -0800

    done TimeHelper and its unit tester

---
+++ src/api/TimeHelper.js
@@ -0,0 +1,165 @@
+/*
+ * @author: Yiming Cai
+ */
+
+// Note that all the functions below might throw errors if invalid inputs are detected
+// They might also return nonsensical outputs depending on how broken the inputs are
+
+// return an array of 7 elements of true/false, starting sunday to saturday
+// Example: input "MWF", return [f, t, f, t, f, t, f], where f is false and t is true
+//                               Su M  Th W  Th F  Sa
+export function get_weekday_array(weekday_string)
+{
+    if (typeof weekday_string !== 'string')
+    {
+        throw new Error("Param passed in get_weekday_array is not a String", 21);
+    }
+
+    var s_days = weekday_string;
+    var dayArray = [false, false, false, false, false, false, false];
+
+    // For each letter in the days string
+    for (var j=0; j < s_days.length; j++) {
+        if (s_days[j] === 'M') {
+            dayArray[1] = true;
+        }
+        else if (s_days[j] === 'T') {
+            j++;
+            if (s_days[j] === 'u') {
+                dayArray[2]= true;
+            }
+            else if (s_days[j] === 'h') {
+                dayArray[4]= true;
+            }
+        }
+        else if (s_days[j] === 'W') {
+            dayArray[3]= true;
+        }
+        else if (s_days[j] === 'F') {
+            dayArray[5]= true;
+        }
+        else if (s_days[j] === 'S') {
+            j++;
+            if (s_days[j] === 'a') {
+                dayArray[6]= true;
+            }
+            else if (s_days[j] === 'u') {
+                dayArray[0]= true;
+            }
+        }
+    }
+
+    return dayArray;
+}
+
+// Takes in 3 string in the format "hhmm" (must be length 4)
+// return (time-range_begin)/(range_end-range_begin) percentage (between 0 and 1)
+export function get_time_percentage(time, range_begin, range_end)
+{
+    if (typeof time !== 'string' || typeof range_begin !== 'string' || typeof range_end !== 'string')
+    {
+        throw new Error("Param passed in get_time_percentage is not a String", 22);
+    }
+
+    var timeh = parseInt(time.substring(0,2), 10);
+    var timem = parseInt(time.substring(2,4), 10);
+    var beginh = parseInt(range_begin.substring(0,2), 10);
+    var beginm = parseInt(range_begin.substring(2,4), 10);
+    var endh = parseInt(range_end.substring(0,2), 10);
+    var endm = parseInt(range_end.substring(2,4), 10);
+
+    var beginVal = beginh * 60 + beginm;
+    var endVal = endh * 60 + endm;
+    var timeVal = timeh * 60 + timem;
+
+    if (beginVal > timeVal || timeVal > endVal)
+    {
+        throw new Error("get_time_percentage: time is not between range_begin and range_end", 23);
+    }
+
+    return (timeVal-beginVal)/(endVal - beginVal);
+}
+
+// Example: Takes in a string of the format "3:41 PM" and covert it to "1541"
+// Returning a string, not a number
+// It assumes that the string passed in has at least 2 numbers separated by a ":"
+//      and that if it after 1159 there will be a 'pm' (case insensitive) somewhere in the string
+// Note that "12:xx PM" is converted to 12xx
+// and that "12:xx AM" is converted to 00xx
+export function get_time_numeric(timestring)
+{
+    if (typeof timestring !== 'string')
+    {
+        throw new Error("Param passed in get_time_numeric is not a String", 24);
+    }
+
+    var s_time = timestring;
+    var hour = ""
+    var min = ""
+    var pm = false;
+    var is_min = false;
+
+    for (var i = 0; i < s_time.length; i++)
+    {
+        if (s_time[i] >= '0' && s_time[i] <= '9')
+        {
+            if (is_min)
+            {
+                min += s_time[i];
+            }
+            else {
+                hour += s_time[i];
+            }
+        }
+        else if (s_time[i] === ':')
+        {
+            is_min = true;
+        }
+        else if (s_time[i].toLowerCase() === 'p')
+        {
+            if (i+1 < s_time.length)
+            {
+                if (s_time[i+1].toLowerCase() === 'm')
+                {
+                    pm = true;
+                }
+            }
+        }
+    }
+
+    var n_hour = parseInt(hour,10);
+    var n_min = parseInt(min,10);
+    if (pm)
+    {
+        // avoid 12pm -> 24
+        if (n_hour !== 12)
+            n_hour += 12;
+    }
+    if (!pm)
+    {
+        if (n_hour == 12)
+        {
+            n_hour = 0;
+        }
+    }
+
+    if (n_hour < 10)
+    {
+        hour = '0' + n_hour.toString();
+    }
+    else
+    {
+        hour = n_hour.toString();
+    }
+
+    if (n_min < 10)
+    {
+        min = '0' + n_min.toString();
+    }
+    else
+    {
+        min = n_min.toString();
+    }
+
+    return hour+min;
+}
--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ import MainLayout from './view/MainLayout';
 import registerServiceWorker from './registerServiceWorker';
 
 // eslint-disable-next-line
-import {test_most_popular_in_list} from './test/TestMostPopularInList'
+import {test_most_popular_in_list} from './test/TestMostPopularInList';
 
 // eslint-disable-next-line
-import CalendarHelper from './api/CalendarHelper'
+import CalendarHelper from './api/CalendarHelper';
+
+// eslint-disable-next-line
+import {test_time_helper} from "./test/TestTimeHelper";
 
 /* firebase config and setup */
 let config = {
@@ -39,14 +42,15 @@ firebase.auth().onAuthStateChanged((user) => {
 * */
 // test_most_popular_in_list();
 
+/* TODO: Run this function to check for the correctness of the TimeHelper functions */
+//test_time_helper();
+
 /*
 * TODO: To test the calendar, comment out Line 15-35, uncomment out the following lines of code
  */
-/*
-var events = [
-    {"days":"MWF", "hours":["11:30", "12:30"], "title":"CSE11" },
-    {"days":"TuTh", "hours":["10:20", "11:20"], "title":"CSE100"},
-    {"days":"MWTu", "hours":["00:10", "11:00"], "title":"Chilling"}
-];
-ReactDOM.render(<CalendarHelper events={events}/>, document.getElementById('root'));
-*/
+// var events = [
+//     {"days":"MWF", "hours":["11:30", "12:30"], "title":"CSE11" },
+//     {"days":"TuTh", "hours":["10:20", "11:20"], "title":"CSE100"},
+//     {"days":"MWTu", "hours":["00:10", "11:00"], "title":"Chilling"}
+// ];
+// ReactDOM.render(<CalendarHelper events={events}/>, document.getElementById('root'));
