f3d020b5e3d3a07b3b0bb28f76f088b4a80b645a
DanieleHuang <32522357+DanieleHuang@users.noreply.github.com>
2017-11-15 19:42:40 -0800

    EnrollmentManager, rewritten - Dan

--- src/dao/EnrollmentManager.js
+++ src/dao/EnrollmentManager.js
@@ -1,22 +1,43 @@
 import Enrollment from './Enrollment'
 
 export function lookup_enrollment_by_id(course_id, callback){
-    let listOfUsers = firebase.database().ref('Enrollment').child(course_id);
-    callback(null, listOfUsers);
+    let db = firebase.database()
+    db.ref('Enrollment/'+ course_id).on('value').then(function (snapshot){
+        let content = snapshot.val();
+
+        //course not found
+        if( content === null ){
+            callback( {err: "Course not found"}, null);
+        }
+
+        //course is found
+        else {
+
+            callback(null, content.enrolled_users);
+        }
+    })
 }
+
+
 
 /*
 * add_user(user_id, course_id);
 * where user_id is the user to add to the course
 * there is no callback because it will always succeed */
 export function add_user(user_id, course_id){
-    //get enrollment object associated with course_id
-    let course = firebase.database().ref('Enrollment').child(course_id);
 
-    let listOfUsers = course.enrolled_users;
-    //push in user_id
-    listOfUsers.push(user_id);
-    course.push();
+    //delegate to lookup
+    lookup_enrollment_by_id( course_id , function ( err, list ){
+
+        //add user to list as property, where key is the user id and value is true
+        list.user_id = true;
+        let enrollment = new Enrollment( course_id, list);
+
+        //update course in db
+        enrollment.push();
+    })
+
+
 
 }
 
@@ -28,49 +49,50 @@ export function add_user(user_id, course_id){
 *        { msg: "Enrollment.remove_user(): user_id DNE" } - if error (in this case user_id not in enrolled_users)
 *   data: null - if any error
 *         { msg: "Enrollment.remove_user(): remove success" } - if no error */
-export function remove_user(user_id, course_id, callback){
-    let course = firebase.database().ref('Enrollment').child(course_id);
-
-    let listOfUsers = course.enrolled_users;
-    let indexOfUser = listOfUsers.indexOf(user_id);
-    let errMessage = "";
-    let successMsg = "";
-    if( indexOfUser === -1 ){
-        errMessage = 'Enrollment.remove)user(): user_id DNE';
-        successMsg = null;
-    }
-    else{
-        //remove one element ( the selected user ) from the list at given index
-        listOfUsers.splice(indexOfUser,1);
-        errMessage = null;
-        successMsg = 'Enrollment.remove_user(): remove success';
-        //update
-        firebase.database().ref('Enrollment').child(course_id).set({
-            course_id: this.course_id,
-            enrolled_users: listOfUsers
-        });
-    }
-    callback( errMessage, successMsg);
-
-
-
+export function remove_user(user_id, course_id, callback) {
+    is_enrolled(user_id, course_id, function (err, data) {
+        //if user is enrolled
+        if (data.value) {
+            //delegate to lookup to find course
+            lookup_enrollment_by_id(course_id, function (err, list) {
+                //remove user
+                list.user_id = null;
+                let enrollment = new Enrollment(course_id, list);
+                enrollment.push();
+                callback(null, {msg: "Enrollment.remove_user(): remove success"});
+            })
+        }
+        //user is not enrolled
+        else {
+            callback({msg: "Enrollment.remove_user(): user_id DNE"}, null)
+        }
+    })
 }
-
-
 
 /* callback (err,data)
 *  err: null // there is no error
 *  data: true or false */
 export function is_enrolled(user_id, course_id, callback){
-    let enrolledList = [];
-    lookup_enrollment_by_id(course_id, function( list){
-        enrolledList = list;
-    });
-    let data = true;
-    if( enrolledList.indexOf(user_id) === -1){
-        data = false;
-    }
-    callback(null, data );
+    let enrolledList = {};
+
+    //delegate to lookup
+    lookup_enrollment_by_id(course_id, function( err, list) {
+
+        //handle errors
+        if (err) {
+            //pass in err thrown by lookup
+            callback(err, null);
+        }
+        else {
+            enrolledList = list;
+            let data = { value: false};
+            if (user_id in enrolledList) {
+                data.value = true;
+            }
+            callback(null, data);
+        }
+
+    })
 }
 
 
