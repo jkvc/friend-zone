63be9d9dc8539e60a8f0d42e68bf9a9d503f829e
CniveK <kevinehc@gmail.com>
2017-11-19 16:48:05 -0800

    added other profile

--- src/view/profile/OtherProfile.js
+++ src/view/profile/OtherProfile.js
@@ -1,7 +1,26 @@
 import React, {Component} from 'react';
+import {get_friend_profiles} from "../../api/StaticData";
 
 
-class OtherProfile extends Component{
+class OtherProfile extends Component {
 
+    constructor(props) {
+        super(props);
+        this.title = "OtherProfile.js"
+        this.state = {
+            profile_obj: get_friend_profiles()[props.user_id]
+        }
+    }
 
+    render() {
+        return (
+            <div>
+                <h1>{this.title}</h1>
+                <pre>{JSON.stringify(this.state, null, 2)}</pre>
+            </div>
+        )
+    }
 }
+
+
+export default OtherProfile;
