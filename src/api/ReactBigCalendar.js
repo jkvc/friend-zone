ff1bce37f345bb6f4a453fcca87f3aac1508a362
yiming-cai <32525453+yiming-cai@users.noreply.github.com>
2017-11-21 17:50:59 -0800

    Added documentation on how to use react-big-calendar

--- src/api/ReactBigCalendar.js
+++ src/api/ReactBigCalendar.js
@@ -1,3 +1,8 @@
+/*
+ * @author: Yiming Cai
+ */
+
+
 import React, {Component} from 'react';
 import BigCalendar from 'react-big-calendar';
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -9,7 +14,17 @@ BigCalendar.setLocalizer(
 
 let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
 
-
+// To use this: make sure you have installed react-big-calendar and moment in the work directory
+// You can do this by running:
+//      npm install react-big-calendar --save
+//      npm install moment --save
+// at the root directory of the project (inside friend-zone)
+//
+// In order to react the calendar, simply use a ReactDOM.render( <BasicCalendar />, document.getElementById(... ) );
+// The styles of the calendar are found in "friend-zone/node-module/react-big-calendar/lib/css/react-big-calendar.css"
+// For more information, go to http://intljusticemission.github.io/react-big-calendar/examples/index.html
+//      and scroll down to find the official documentations
+// For source code example, go to https://github.com/intljusticemission/react-big-calendar
 class BasicCalendar extends Component{
 
     render(){
@@ -17,18 +32,19 @@ class BasicCalendar extends Component{
             <BigCalendar
                 {...this.props}
                 events={  [{
-                    'title': 'Late Night Event',
-                    'start':new Date(2015, 3, 17, 19, 30, 0),
-                    'end': new Date(2015, 3, 17, 22, 0, 0)
-                },
+                        'title': 'Late Night Event',
+                        'start':new Date(2017, 11, 21, 19, 30, 0),
+                        'end': new Date(2017, 11, 21, 22, 0, 0)
+                    },
                     {
                         'title': 'Another',
-                        'start': new Date(2015, 3, 17, 20, 0, 0),
-                        'end': new Date(2015, 3, 17, 23, 0, 0)
+                        'start': new Date(2017, 11, 21, 20, 0, 0),
+                        'end': new Date(2017, 11, 21, 23, 0, 0)
                     }] }
                 views={allViews}
                 step={60}
-                defaultDate={new Date(2015, 3, 1)}
+                // Be default this should return current date
+                defaultDate={new Date()}
             />
         )
     }
